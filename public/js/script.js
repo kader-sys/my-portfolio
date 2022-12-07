7767dba16f5c6e9abd61d71f0e8bb82df0a2d1ef
kader hammouya <kaderderk98@gmail.com>
2022-12-07 18:25:04 +0100

    chane tim of spinner

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function() {
     document.querySelector(".preloader").classList.add("opacity-0");
     setTimeout(function() {
         document.querySelector(".preloader").style.display = "none";
-    }, 1000);
+    }, 5000);
 })
 
 
