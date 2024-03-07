b42e0bb666996e0bc7012e88f792e725e85d2a67
kader hammouya <kaderderk98@gmail.com>
2024-03-07 19:35:22 +0100

    set blue color as defult

--- public/js/styleSwitcher.js
+++ public/js/styleSwitcher.js
@@ -2,30 +2,31 @@ const links = document.querySelectorAll(".alternate-style");
 totalLinks = links.length;
 
 function setActiveStyle(color) {
-    for (let i = 0; i < totalLinks; i++) {
-        if (color === links[i].getAttribute("title")) {
-            links[i].removeAttribute("disabled");
-        } else {
-            links[i].setAttribute("disabled", "true");
-        }
-
+  for (let i = 0; i < totalLinks; i++) {
+    if (color === links[i].getAttribute("title")) {
+      links[i].removeAttribute("disabled");
+    } else {
+      links[i].setAttribute("disabled", "true");
     }
+  }
 }
 
 // body skin
 const bodySkin = document.querySelectorAll(".body-skin"),
-    totalBodySkin = bodySkin.length;
+  totalBodySkin = bodySkin.length;
 
 for (let i = 0; i < totalBodySkin; i++) {
-    bodySkin[i].addEventListener("change", function() {
-        if (this.value === "dark") {
-            document.body.classList.add("dark");
-        } else {
-            document.body.classList.remove("dark");
-        }
-    })
+  bodySkin[i].addEventListener("change", function () {
+    if (this.value === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  });
 }
 
-document.querySelector(".toggle-style-switcher").addEventListener("click", () => {
+document
+  .querySelector(".toggle-style-switcher")
+  .addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
-})
+  });
